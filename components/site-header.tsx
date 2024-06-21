883bf137b0cee60961f31c15b0e35a3db553f71a
DarkInventor <ktmehta25@gmail.com>
2024-06-21 11:06:11 -0400

    make nav icons visible

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -18,7 +18,7 @@ export function SiteHeader() {
         <MainNav />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-6">
-            <div className="hidden sm:block">
+            <div className="sm:block">
              
 
               {/* <AnimatedSubscribeButton
