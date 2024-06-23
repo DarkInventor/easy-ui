bd60c8cd628602d49cf2b993a59e0b6a42e256f6
DarkInventor <ktmehta25@gmail.com>
2024-06-23 13:16:04 -0400

    lodo --svg r2

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -16,7 +16,7 @@ export function MainNav() {
   return (
     <div className="mr-4 md:flex">
       <Link href="/" className="mr-6 flex items-center space-x-2">
-        <img src="/logo.svg" className="size-7" alt="Logo" />
+        <img src="https://pub-0cd6f9d4131f4f79ac40219248ae64db.r2.dev/logo.svg" className="size-7" alt="Logo" />
         <span className="font-bold sm:inline-block">
           {siteConfig.name}
         </span>
