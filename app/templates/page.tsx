4e45353b2451f5df65cfbe339b77d9df9d2548e7
DarkInventor <ktmehta25@gmail.com>
2024-08-05 11:52:55 -0400

    add easy-tmp-3

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -241,6 +241,16 @@ export default function TemplatePage() {
           <p className="text-base text-gray-600 dark:text-gray-400">Easy Shadcn Landing Page Template</p>
         </MagicCard>
         <MagicCard
+          className="group cursor-pointer flex flex-col items-center justify-center overflow-hidden p-6 lg:p-10 sm:p-0 md:p-10 shadow-2xl"  
+          onClick={() => window.location.href='/ez-tmp3'}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <img src="/ez-tmp3.png" alt="EasyWaitlist" className="w-full h-auto" />
+          <h2 className="mt-4 text-lg font-bold text-gray-800 dark:text-gray-200">Easy Template 3</h2>
+          <p className="text-base text-gray-600 dark:text-gray-400">Easy Aceternity UI Landing Page Template</p>
+        </MagicCard>
+        <MagicCard
           className="group cursor-pointer flex flex-col items-center justify-center overflow-hidden p-10 shadow-2xl"
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
