ec0dfd049db684639049189918ed40cb104c439b
DarkInventor <ktmehta25@gmail.com>
2025-02-12 03:23:29 -0500

    update sitemap path

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://easyui.pro',
+    sitemap: 'https://easyui.pro/sitemap.xml',
   }
 }
